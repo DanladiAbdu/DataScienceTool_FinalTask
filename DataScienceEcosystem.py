7871823881728b2e7a4613a2e00591b1b510a51e
DanladiAbdu <112549935+DanladiAbdu@users.noreply.github.com>
2024-01-23 11:30:13 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives**
+# + List popular Data Science Languages.
+# + List some commonly used libraries by Data Scientists.
+# + Create a single column table.
+# + Perform arithmetic expression in Python.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Numpy
+# 3. Scikitlearn
+
+# | Data Science Tool |
+# |-------------------|
+# | RStudio           |
+# | Apache Spark      |
+# | TensorFlow        |
+
+# ### Below are few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# In[7]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+a = 200/60
+print(int(a),"hr")
+
+
+# ## Author
+# Danladi Mamman Abdu
+
+# In[ ]:
+
+
+
+
